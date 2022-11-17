7b7623617858fd38bee4956d8613aa6216ec2b4f
Mohammed Alkordy <moda-233@hotmail.com>
2022-11-17 13:56:05 +0300

    Font repair

--- js/script.js
+++ js/script.js
@@ -19,12 +19,24 @@ const setLanguage = (language) => {
     document.getElementById("Content").setAttribute("dir" , "rtl");
     document.getElementById("Content").classList.add('ar');
     document.getElementById("Content").classList.remove('Content');
+    document.getElementById("txt").classList.add('txt-ar');
+    document.getElementById("txt").classList.remove('txt');
+    document.getElementById("sec-text").classList.add('sec-text-ar');
+    document.getElementById("sec-text").classList.remove('sec-text');
+    document.getElementById("sub-text").classList.add('sub-text-ar');
+    document.getElementById("sub-text").classList.remove('sub-text');
     document.querySelector("form").innerHTML=`<input type="email" name="Email" placeholder="سّجل من خلال بريدك الإلكتروني" class="input-ar">
     <input type="submit" value="اشترك الآن" class="sub-ar">`;
   }else{
     document.getElementById("Content").setAttribute("dir" , "ltr");
     document.getElementById("Content").classList.add('Content');
     document.getElementById("Content").classList.remove('ar');
+    document.getElementById("txt").classList.add('txt');
+    document.getElementById("txt").classList.remove('txt-ar');
+    document.getElementById("sec-text").classList.add('sec-text');
+    document.getElementById("sec-text").classList.remove('sec-text-ar');
+    document.getElementById("sub-text").classList.add('sub-text');
+    document.getElementById("sub-text").classList.remove('sub-text-ar');
     document.querySelector("form").innerHTML=`<input type="email" name="Email" placeholder="Enter your E-mail" class="input">
     <input type="submit" value="Subscribe" class="sub">`;
   }
